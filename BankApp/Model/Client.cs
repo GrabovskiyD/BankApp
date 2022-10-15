3c27661d03d109e4e129ad12cf59b9254b50a09e
Dmitriy Grabovskiy <dimokurl@gmail.com>
2022-10-15 19:09:00 +0300

    Created a Client class

---
+++ BankApp/Model/Client.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SQLite;
+
+namespace BankApp.Model
+{
+    public class Client
+    {
+        [PrimaryKey, AutoIncrement]
+        public int Id { get; set; }
+        [MaxLength(30)]
+        public string LastName { get; set; }
+        [MaxLength(30)]
+        public string FirstName { get; set; }
+        [MaxLength(30)]
+        public string MiddleName { get; set; }
+        [MaxLength(11)]
+        public string PhoneNumber { get; set; }
+        public string PassportSeries { get; set; }
+        public string PassportNumber { get; set; }
+    }
+}
